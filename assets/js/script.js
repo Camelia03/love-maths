9fd16afa3cf7f0bc2118ead7df11ae1d880254e9
Camelia03 <camelia_weber@yahoo.com>
2023-02-12 12:05:38 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incremnetWrongAnswer() {
+
+}
+
+function AdditionQuestion() {
+
+}
+
+function displaySubstractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
